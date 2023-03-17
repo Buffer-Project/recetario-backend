97d33e96286fb8e3b364507f00c0748447374082
Tuckson <franrache@gmail.com>
2023-03-17 15:50:57 -0300

    WIP: feature: delete calif segun el id de la calif

--- src/main/java/com/buffer/recetariobackend/controller/CalificacionController.java
+++ src/main/java/com/buffer/recetariobackend/controller/CalificacionController.java
@@ -3,6 +3,7 @@ package com.buffer.recetariobackend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.buffer.recetariobackend.entity.Calificacion;
+import com.buffer.recetariobackend.entity.Receta;
 import com.buffer.recetariobackend.service.ICalificacionService;
 
 @CrossOrigin()
@@ -20,11 +22,21 @@ public class CalificacionController {
 
   @Autowired
     private ICalificacionService calificacionService;
+
  
   @PostMapping("/{id}")
-  public ResponseEntity<Calificacion> calificar(@PathVariable String id, @RequestBody Calificacion Calificacion ){
-    return null;
+  public ResponseEntity<Receta> calificar(@PathVariable String id, @RequestBody Calificacion Calificacion ){
+     Receta recetaConCalificacionNueva = calificacionService.calificar(id, Calificacion);
+
+     if(recetaConCalificacionNueva == null){
+      return ResponseEntity.notFound().build();
+     }
+
+      return ResponseEntity.ok(recetaConCalificacionNueva);
   }
+
+  
+
         
       
     
