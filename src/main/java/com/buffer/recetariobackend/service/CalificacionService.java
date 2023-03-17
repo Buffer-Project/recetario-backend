590c58ea5839f850e3ae6833d12c7e1a119cfe21
Tuckson <franrache@gmail.com>
2023-03-17 15:52:00 -0300

    WIP: feature: avance con endpoint delete calif

--- src/main/java/com/buffer/recetariobackend/service/CalificacionService.java
+++ src/main/java/com/buffer/recetariobackend/service/CalificacionService.java
@@ -1,7 +1,9 @@
 package com.buffer.recetariobackend.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,16 +27,31 @@ public class CalificacionService implements ICalificacionService {
     public Receta calificar(String id, Calificacion calificacion) {
 
         Optional<Receta> recetaConCalificaciones = recetasService.getRecetaById(id);
-
         Receta receta = recetaConCalificaciones.get();
-
         List<Calificacion> calificaciones = receta.getCalificaciones();
-
         calificaciones.add(calificacion);
-
-        receta.updateReceta(receta);
-
+        recetasService.updateReceta(receta);
         return receta;
+
+    }
+
+    @Override
+    public Receta deleteCalificacionByIdCalificacion(String idReceta, String idCalificacion) {
+        
+        Optional<Receta> receta = recetasService.getRecetaById(idReceta);
+        Receta recetaFinal = receta.get();
+        List<Calificacion> calificaciones = recetaFinal.getCalificaciones();
+        List<Calificacion> listaFinal = new ArrayList<>();
+
+        for (Calificacion calif : calificaciones) {
+            if (calif.getIdCalificacion() != idCalificacion) {
+                listaFinal.add(calif);
+            }
+        }
+
+        recetaFinal.setCalificaciones(listaFinal);
+        recetasService.updateReceta(recetaFinal);
+        return recetaFinal;
 
     }
 
--- src/main/java/com/buffer/recetariobackend/service/ICalificacionService.java
+++ src/main/java/com/buffer/recetariobackend/service/ICalificacionService.java
@@ -1,5 +1,6 @@
 package com.buffer.recetariobackend.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.buffer.recetariobackend.entity.Calificacion;
@@ -10,5 +11,5 @@ public interface ICalificacionService {
 
     public Receta calificar(String id, Calificacion calificacion);
 
-
+    public Receta deleteCalificacionByIdCalificacion(String idReceta, String idCalificacion);
 }
