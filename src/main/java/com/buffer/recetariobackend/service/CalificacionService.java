7b6b9b7592fc3416f220e1e5502ce915dd195e10
Tuckson <franrache@gmail.com>
2023-03-27 16:59:38 -0300

    refactor: modificar calificacion (aun falta testing)

--- src/main/java/com/buffer/recetariobackend/service/CalificacionService.java
+++ src/main/java/com/buffer/recetariobackend/service/CalificacionService.java
@@ -1,12 +1,9 @@
 package com.buffer.recetariobackend.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import com.buffer.recetariobackend.entity.Calificacion;
 import com.buffer.recetariobackend.entity.Receta;
 import com.buffer.recetariobackend.exception.RecetaNotFoundException;
@@ -16,38 +13,7 @@ public class CalificacionService implements ICalificacionService {
 
     @Autowired
     private IRecetasService recetasService;
-
-    @Override
-    public Receta calificar(String id, Calificacion calificacion) {
-
-        Optional<Receta> recetaConCalificaciones = recetasService.getRecetaById(id);
-        Receta receta = recetaConCalificaciones.get();
-        List<Calificacion> calificaciones = receta.getCalificaciones();
-        calificaciones.add(calificacion);
-        recetasService.updateReceta(receta);
-        return receta;
-
-    }
-
-    @Override
-    public Receta deleteCalificacionByIdCalificacion(String idReceta, String idCalificacion) {
-
-        Optional<Receta> receta = recetasService.getRecetaById(idReceta);
-        Receta recetaFinal = receta.get();
-        List<Calificacion> calificaciones = recetaFinal.getCalificaciones();
-        List<Calificacion> listaFinal = new ArrayList<>();
-
-        for (Calificacion calif : calificaciones) {
-            if (calif.getIdCalificacion() != idCalificacion) {
-                listaFinal.add(calif);
-            }
-        }
-
-        recetaFinal.setCalificaciones(listaFinal);
-        recetasService.updateReceta(recetaFinal);
-        return recetaFinal;
-
-    }
+  
 
     @Override
     public Receta modificarCalificacion(String idReceta, Calificacion calificacion) {
--- src/main/java/com/buffer/recetariobackend/service/ICalificacionService.java
+++ src/main/java/com/buffer/recetariobackend/service/ICalificacionService.java
@@ -5,9 +5,5 @@ import com.buffer.recetariobackend.entity.Receta;
 
 public interface ICalificacionService {
 
-    public Receta calificar(String id, Calificacion calificacion);
-
-    public Receta deleteCalificacionByIdCalificacion(String idReceta, String idCalificacion);
-
     public Receta modificarCalificacion(String idReceta, Calificacion calificacion);
 }
