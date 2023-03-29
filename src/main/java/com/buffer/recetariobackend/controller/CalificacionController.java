762d1be8c49bd3c927cb7699b5d42459cbaea24a
Germán Altairac <santiago.altairac@kinandcarta.com>
2023-03-29 11:55:24 -0300

    fix: califService interface merge

--- src/main/java/com/buffer/recetariobackend/controller/CalificacionController.java
+++ src/main/java/com/buffer/recetariobackend/controller/CalificacionController.java
@@ -1,14 +1,9 @@
 package com.buffer.recetariobackend.controller;
 
+import com.buffer.recetariobackend.entity.Calificacion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.buffer.recetariobackend.entity.Receta;
 import com.buffer.recetariobackend.exception.RecetaNotFoundException;
--- src/main/java/com/buffer/recetariobackend/service/ICalificacionService.java
+++ src/main/java/com/buffer/recetariobackend/service/ICalificacionService.java
@@ -6,4 +6,8 @@ import com.buffer.recetariobackend.entity.Receta;
 public interface ICalificacionService {
 
     public Receta modificarCalificacion(String idReceta, Calificacion calificacion);
+
+    Receta deleteCalificacionByIdCalificacion(String idReceta, String idCalificacion);
+
+    Receta calificar(String id, Calificacion calificacion);
 }
